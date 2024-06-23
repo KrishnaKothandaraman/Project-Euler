b611fb7d95466b669f7babfd370dec1349cba3f9
KrishnaKothandaraman <kkrishna0701@gmail.com>
2024-06-23 22:35:13 +0800

    Rule 110 in py

--- python/1-10.py
+++ python/1-10.py
@@ -1,4 +1,39 @@
 
+def rule110(n):
+    table = {
+        0 : 0,
+        1 : 1,
+        2 : 1,
+        3 : 1,
+        4 : 0,
+        5 : 1,
+        6 : 1,
+        7 : 0
+    }
+    board = []
+    for _ in range(2):
+        board.append([0]*n)
+
+    board[0][n-2] = 1
+    cur_board = 0
+    for _ in range(n): 
+        for i in range(n):
+            if board[cur_board][i] == 0:
+                print(" ",end = "")
+            else:
+                print("*", end ="")
+        print()
+
+        for i in range(n):
+            a = board[cur_board][i-1]
+            b = board[cur_board][i]
+            c = board[cur_board][(i+1) % (n)]
+            triplet = (a << 2) | (b << 1) | (c << 0)
+            board[1-cur_board][i] = table[triplet]
+            #print(f"Setting board {1-cur_board} = {table[val]}")
+        
+        cur_board = 1 - cur_board
+
 
 def p10(n):
     x = [(i, 1) for i in range(n+1)]
@@ -14,5 +49,3 @@ def p10(n):
         for j in range(x[i][0]**2, n+1, x[i][0]):
             x[j] = (x[j][0], 0)
     return s
-
-print(p10(2_000_000))
